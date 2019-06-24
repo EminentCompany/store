b8029f06523ec250ddb83d8dd92107ee6024dc2c
Eminent <51912132+EminentCompany@users.noreply.github.com>
2019-06-24 11:04:21 +0200

    Commting Source Code

---
+++ app/src/main/java/com/eminent/a2019/eminentstore/MainActivity.java
@@ -0,0 +1,39 @@
+package com.eminent.a2019.eminentstore;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button PADD;
+    Button PREVIEW;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PADD = (Button) findViewById(R.id.PADD);
+        PADD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,Add_Product.class);
+                startActivity(intent);
+            }
+        });
+
+        PREVIEW =(Button)findViewById(R.id.PREVIEW);
+        PREVIEW.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Product_Review.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
